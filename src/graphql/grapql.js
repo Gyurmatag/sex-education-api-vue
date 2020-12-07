b6c895b6253198a40ec6e268451cee193743362f
Varga György <gyurmatag@gmail.com>
2020-12-07 17:59:04 +0100

    refreshToken mókolások

--- src/graphql/grapql.js
+++ src/graphql/grapql.js
@@ -2,6 +2,8 @@ import { ApolloClient, ApolloLink, createHttpLink } from '@apollo/client/core'
 import { onError } from '@apollo/client/link/error'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import authDataLocalQuery from '@/graphql/queries/authDataLocal.query.gql'
+import refreshAccessTokenMutation from '@/graphql/mutations/refreshAccessToken.mutation.gql'
+import router from '@/router'
 
 const httpLink = createHttpLink({
   uri: process.env.VUE_APP_BACKEND_BASE_URI
@@ -10,12 +12,43 @@ const httpLink = createHttpLink({
 const cache = new InMemoryCache()
 cache.writeData({ data: { authDataLocal: null } })
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
+const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
   if (graphQLErrors) {
-    graphQLErrors.map(({ extensions, message, locations, path }) =>
+    for (const err of graphQLErrors) {
       // eslint-disable-next-line no-console
-      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-    )
+      console.log(`[GraphQL error]: Message: ${err.message}, Path: ${err.path}`)
+      if (err.extensions.code === 'UNAUTHENTICATED') {
+        const data = cache.readQuery({ query: authDataLocalQuery })
+
+        if (data.authDataLocal?.refreshToken) {
+          apolloClient.mutate({
+            mutation: refreshAccessTokenMutation,
+            variables: {
+              availableRefreshToken: data.authDataLocal.refreshToken
+            }
+          }).then(res => {
+            const refreshedAuthData = res.data.refreshAccessToken
+            data.authDataLocal = {
+              ...refreshedAuthData
+            }
+            cache.writeQuery({ query: authDataLocalQuery, data })
+            const oldHeaders = operation.getContext().headers
+            operation.setContext({
+              headers: {
+                ...oldHeaders,
+                'x-access-token': data.authDataLocal.accessToken || null
+              }
+            })
+            // FIXME: itt valami nem jó (nem ismétli meg a kérést)
+            return forward(operation)
+          })
+            .catch(_ => {
+              cache.writeQuery({ query: authDataLocalQuery, data: null })
+              router.push('/auth/login')
+            })
+        }
+      }
+    }
   }
 
   if (networkError) {
@@ -32,7 +65,6 @@ const authLink = new ApolloLink((operation, forward) => {
       'x-access-token': accessToken || null
     }
   })
-
   return forward(operation)
 })
 
