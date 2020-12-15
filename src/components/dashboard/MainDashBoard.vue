8664c36af69c4367de0b096bddb6479bd371f11c
Varga György <gyorgy.varga@icellmobilsoft.hu>
2020-12-15 10:45:00 +0100

    admin akciók elválasztása + refakt

--- src/components/dashboard/MainDashBoard.vue
+++ src/components/dashboard/MainDashBoard.vue
@@ -3,29 +3,36 @@
     <div class="flex justify-center py-5 bg-gray-800 text-xl text-white">
       {{ $t('dashBoard.title') }}
     </div>
-    <div class="flex justify-center space-x-6 p-6">
+    <div
+      v-if="authData.authDataLocal.user.role === 'ADMIN'"
+      class="flex justify-center space-x-6 p-6"
+    >
       <admin-actions />
     </div>
     <div class="flex justify-center space-x-6 p-6">
-      <user-actions />
+      <logout-action />
     </div>
   </div>
 </template>
 
 <script>
 import AdminActions from '@/components/dashboard/admin/AdminActions'
-import UserActions from '@/components/dashboard/user/UserActions'
+import LogoutAction from '@/components/dashboard/shared/LogoutAction'
+import apolloClient from '@/graphql/grapql'
+import authDataLocalQuery from '@/graphql/queries/local/authDataLocal.query.gql'
 
 export default {
   name: 'MainDashBoard',
 
   components: {
     AdminActions,
-    UserActions
+    LogoutAction
+  },
+
+  setup () {
+    const authData = apolloClient.cache.readQuery({ query: authDataLocalQuery })
+
+    return { authData }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
